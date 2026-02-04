19e8827c2c190ae7a44ff86f62b094fc9ce091f0
Muhammad Firas <muhfiras1@gmail.com>
2026-02-04 20:12:45 +0700

    fix: networking module

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -3,25 +3,7 @@
 {
   # Enable networking via WiFi
   networking = {
-    wireless = {
-      enable = true;
-
-      # Replace with your WiFi name and password
-      networks = {
-        "YourWiFiName" = {
-          psk = "your-wifi-password";
-        };
-      };
-    };
-
-    # Static IP (recommended for servers)
-    # Uncomment and adjust:
-    # interfaces.wlp2s0.ipv4.addresses = [{
-    #   address = "192.168.1.50";
-    #   prefixLength = 24;
-    # }];
-    # defaultGateway = "192.168.1.1";
-    # nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    networkmanager.enable = true;
 
     # Firewall
     firewall = {
