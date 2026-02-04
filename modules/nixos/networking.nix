b8f660a1fcdf0c9c644023cabc32bb76dfe259fc
Muhammad Firas <muhfiras1@gmail.com>
2026-02-04 22:32:38 +0700

    fix: networking issue

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -7,12 +7,7 @@
     # Disable wpa_supplicant (conflicts with NetworkManager)
     wireless.enable = lib.mkForce false;
 
-    # Static IP
-    interfaces.wlp2s0.ipv4.addresses = [{
-      address = "192.168.100.10";
-      prefixLength = 24;
-    }];
-    defaultGateway = "192.168.100.1";
+    # DNS
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
     # Firewall
