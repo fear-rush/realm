1e0ee027eba384588ffe92ccaad67a05149ff89f
Muhammad Firas <muhfiras1@gmail.com>
2026-02-05 00:44:19 +0700

    fix: immich bind port

--- modules/nixos/immich.nix
+++ modules/nixos/immich.nix
@@ -4,6 +4,7 @@
   # Immich - self-hosted photo & video backup
   services.immich = {
     enable = true;
+    host = "0.0.0.0";
     port = 2283;
     openFirewall = true;
     machine-learning.enable = false;
