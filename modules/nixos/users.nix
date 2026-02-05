f5a7ce6a4c79d8f0190d250f8bd47b700f07c5df
Muhammad Firas <muhfiras1@gmail.com>
2026-02-05 17:54:55 +0700

    chore: add firas-pc ssh pub key

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -16,7 +16,8 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIhjnmrkuXxq0lM2pM2WYy1WcgPXY3CLqO6bhRCd+5u1 firas"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgmoF+cflWZj4a8hdMT94Xiu+/EP0nDhD2d7O5t04rR"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgmoF+cflWZj4a8hdMT94Xiu+/EP0nDhD2d7O5t04rR firas-android"
+      "ssh-ed25519 AAAAC3NzaC1LZDI1NTESAAAAIA/EpQGRwjVCUtukotGDUZkmFGIphk89/m4FoeVtcvsm firas-pc"
     ];
 
     shell = pkgs.bash;
